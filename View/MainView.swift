522a17efadac5ac0f840cd36be0f442e7dd49d23
Yune Cho <fusma159@naver.com>
2025-02-03 23:06:19 +0900

    [REF] Update MainView

--- View/MainView.swift
+++ View/MainView.swift
@@ -7,51 +7,69 @@ struct MainView: View {
     @State private var mainButtonState: MainButtonState = .disabled
     
     var body: some View {
-        VStack {
-            HStack {
+        NavigationStack {
+            VStack {
+                header
+                
                 Spacer()
                 
-                Button {
-                    showInfo = true
-                } label: {
-                    Image(systemName: "info.circle")
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                        .foregroundStyle(.teal)
-                }
+                ProcessCircleView()
+                
+                Spacer()
+                
+                MainButton(
+                    state: mainButtonState,
+                    action: {
+                        if mainButtonState == .play {
+                            mainButtonState = .stop
+                        } else {
+                            mainButtonState = .play
+                        }
+                    }
+                )
+            }
+            .padding(16)
+            .background(.black.opacity(0.95))
+            .onAppear(perform: {
+                mainButtonState = bluetoothConnectionManager.isBluetoothConnected ? .play : .disabled
+            })
+            .onChange(of: bluetoothConnectionManager.isBluetoothConnected) {
+                mainButtonState = bluetoothConnectionManager.isBluetoothConnected ? .play : .disabled
+            }
+            .sheet(isPresented: $showInfo) {
+                InfoView()
+            }
+        }
+    }
+}
+
+extension MainView {
+    private var header: some View {
+        HStack(spacing: 16) {
+            Spacer()
+            
+            Button {
+                showInfo = true
+            } label: {
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(.teal)
             }
             
-            Spacer()
-            
-            //ProgressCircleView
-            
-            Spacer()
-            
-            MainButton(
-                state: mainButtonState,
-                action: {
-                    if mainButtonState == .play {
-                        mainButtonState = .stop
-                    } else {
-                        mainButtonState = .play
-                    }
-                }
-            )
-        }
-        .padding(16)
-        .background(.black.opacity(0.95))
-        .onAppear(perform: {
-            mainButtonState = bluetoothConnectionManager.isBluetoothConnected ? .play : .disabled
-        })
-        .onChange(of: bluetoothConnectionManager.isBluetoothConnected) {
-            mainButtonState = bluetoothConnectionManager.isBluetoothConnected ? .play : .disabled
-        }
-        .sheet(isPresented: $showInfo) {
-            InfoView()
+            NavigationLink {
+                SettingsView()
+            } label: {
+                Image(systemName: "gearshape")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(.teal)
+            }
         }
     }
 }
 
 #Preview {
     MainView()
+        .environment(BluetoothConnectionManager())
 }
---
+++ View/ProcessCircleView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ProcessCircleView: View {
+    var body: some View {
+        Text("ProcessCircleView")
+            .foregroundStyle(.teal)
+    }
+}
+
+#Preview {
+    ProcessCircleView()
+}
