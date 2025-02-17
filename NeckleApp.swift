f04c1cb97673f125f00c5469789134ba1f1fac39
Yune Cho <fusma159@naver.com>
2025-02-17 21:37:45 +0900

    [FEAT] Add AppDelegate

--- NeckleApp.swift
+++ NeckleApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct NeckleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     @State private var userSettingsManager = UserSettingsManager()
     @State private var bluetoothConnectionManager = BluetoothConnectionManager()
     @State private var speechManager = SpeechManager()
