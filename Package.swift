daa2768160f985f47206d388a2215368fe493e58
Yune Cho <fusma159@naver.com>
2025-09-19 20:30:52 +0900

    [REF] Update version info

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "io.github.picturehouse.Neckle",
             teamIdentifier: "WAC2RPX5HC",
-            displayVersion: "1.1.1",
+            displayVersion: "1.1.2",
             bundleVersion: "1",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.teal),
